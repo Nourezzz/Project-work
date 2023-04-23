d8b7a291dcd321c152eabdbb6d864255e5fdbcc8
Noure <101957571+Nourezzz@users.noreply.github.com>
2023-04-24 01:57:50 +0200

    Update AnagraficaService.java

--- src/main/java/it/corso/service/AnagraficaService.java
+++ src/main/java/it/corso/service/AnagraficaService.java
@@ -2,17 +2,15 @@ package it.corso.service;
 
 import java.util.List;
 import it.corso.model.Anagrafica;
-import it.corso.model.Profilo;
 import jakarta.servlet.http.HttpSession;
 
 
 public interface AnagraficaService 
 {
 	void registraAnagrafica(Anagrafica anagrafica);
-	Anagrafica getAnagraficaByID(int id);
+	Anagrafica getAnagraficaById(int id);
 	List<Anagrafica> getAnagrafica();
 	void cancellaAnagrafica(Anagrafica anagrafica);
-	boolean loginUtente(List<Profilo> profili, String username, String password, HttpSession session);
+	boolean loginUtente( String username, String password, HttpSession session);
 	boolean loginAdmin(String username, String password, HttpSession session);
-	String logout(HttpSession session);
 }
