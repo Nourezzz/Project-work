584d4ce2d3346a4bab5b6396d97192a27b8d151c
Noure <101957571+Nourezzz@users.noreply.github.com>
2023-04-24 01:52:51 +0200

    Update PrenotazioneDao.java

--- src/main/java/it/corso/dao/PrenotazioneDao.java
+++ src/main/java/it/corso/dao/PrenotazioneDao.java
@@ -2,8 +2,10 @@ package it.corso.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
+
 import it.corso.model.Prenotazione;
 
-public interface PrenotazioneDao extends CrudRepository<Prenotazione, Integer> {
-
+public interface PrenotazioneDao extends CrudRepository<Prenotazione, Integer> 
+{
+	//Prenotazione findByanagrafica(String nome, String cognome, String data_di_nascita, String email);
 }
