11c3d50b20918e08450538cce3934401f84565d8
Noure <101957571+Nourezzz@users.noreply.github.com>
2023-04-24 01:55:52 +0200

    Update Prenotazione.java

--- src/main/java/it/corso/model/Prenotazione.java
+++ src/main/java/it/corso/model/Prenotazione.java
@@ -2,6 +2,9 @@ package it.corso.model;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -10,7 +13,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity // trasforma la classe cliente nell'Entity cliente , permettendo di essere mappata nella tabella 
@@ -22,25 +24,18 @@ public class Prenotazione
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // Ci adeguiamo al modo del database di gestione della chiave primaria della tabella clienti
 	private int id;
 	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@Column(name = "data")
 	private LocalDate data;
 	
+	@DateTimeFormat(pattern = "HH:mm")
 	@Column(name = "time")
 	private LocalTime ora;
-	
-	@Column(name = "sconto")
-	private double sconto;
 	
 	@ManyToOne(cascade = CascadeType.REFRESH) //refresh mi sta ad indicare che le modifiche che faccio su una determinata tabella hanno effetto solo su quella
 	@JoinColumn(name = "id_anagrafica", referencedColumnName = "id")// Con la joinColumn, vado a relazionare la foreign key della classe dove ci troviamo (ordini)  con la referenceColumnname
 	//della tabella relazionata, ovvero in questo caso l'"id" della tabella "clienti"
 	private Anagrafica anagrafica;// deve avere lo stesso nome dell'attributo nella classe proprietario (Cliente classe)
-	
-	@OneToOne(cascade = CascadeType.REFRESH)
-	@JoinColumn 
-	(name = "id_tatuaggi", referencedColumnName = "id")
-	
-	private Tatuaggio tatuaggio;
 	
 	//getter e setter 
 	public int getId() {
@@ -49,23 +44,11 @@ public class Prenotazione
 	public void setId(int id) {
 		this.id = id;
 	}
-	public double getSconto() {
-		return sconto;
-	}
-	public void setSconto(double sconto) {
-		this.sconto = sconto;
-	}
 	public Anagrafica getAnagrafica() {
 		return anagrafica;
 	}
 	public void setAnagrafica(Anagrafica anagrafica) {
 		this.anagrafica = anagrafica;
-	}
-	public Tatuaggio getTatuaggio() {
-		return tatuaggio;
-	}
-	public void setTatuaggio(Tatuaggio tatuaggio) {
-		this.tatuaggio = tatuaggio;
 	}
 	public LocalDate getData() {
 		return data;
