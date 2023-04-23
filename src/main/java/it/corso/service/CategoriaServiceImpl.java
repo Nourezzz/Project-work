ea6e9725a861dbbb5fafe2fc04a13022bcddef93
Noure <101957571+Nourezzz@users.noreply.github.com>
2023-04-24 01:59:36 +0200

    Create CategoriaServiceImpl.java

---
+++ src/main/java/it/corso/service/CategoriaServiceImpl.java
@@ -0,0 +1,29 @@
+package it.corso.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.corso.dao.CategoriaDao;
+import it.corso.model.Categoria;
+
+@Service
+public class CategoriaServiceImpl implements CategoriaService {
+
+	@Autowired
+	private CategoriaDao categoriaDao;
+	
+	@Override
+	public Categoria getCategoriaById(int id) {
+		
+		return categoriaDao.findById(id).get();
+	}
+	
+	@Override
+	public List<Categoria> getCategoria() {
+		
+		return (List<Categoria>) categoriaDao.findAll();
+	}
+
+}
