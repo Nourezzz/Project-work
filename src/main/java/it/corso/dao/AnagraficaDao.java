93437ff532fa919c1b9837bf831f36ac73f73eb5
Noure <101957571+Nourezzz@users.noreply.github.com>
2023-04-24 01:52:26 +0200

    Update AnagraficaDao.java

--- src/main/java/it/corso/dao/AnagraficaDao.java
+++ src/main/java/it/corso/dao/AnagraficaDao.java
@@ -4,6 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 
 import it.corso.model.Anagrafica;
 
-public interface AnagraficaDao extends CrudRepository<Anagrafica, Integer> {
+public interface AnagraficaDao extends CrudRepository<Anagrafica, Integer> 
+{
+	Anagrafica findByProfiloUsernameAndProfiloPassword(String username, String password);
+	
 	
 }
