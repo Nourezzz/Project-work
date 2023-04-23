0118a9342e1c4253ef894aef1402486940a9213d
Noure <101957571+Nourezzz@users.noreply.github.com>
2023-04-24 01:46:03 +0200

    Update IndexController.java

--- src/main/java/it/corso/controller/IndexController.java
+++ src/main/java/it/corso/controller/IndexController.java
@@ -1,15 +1,24 @@
 package it.corso.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import it.corso.model.Anagrafica;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/homepage")
 public class IndexController {
 
 	@GetMapping
-	public String getPage() {
-		return "index";
+	public String getPage(HttpSession session, Model model) {
+		if(session.getAttribute("profilo") == null)
+            return "index";
+        Anagrafica profilo = (Anagrafica) session.getAttribute("profilo");
+        model.addAttribute("profilo", profilo);
+        return "index";
 	}
+	
 }
