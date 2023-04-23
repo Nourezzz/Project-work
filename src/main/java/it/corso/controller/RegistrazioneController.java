fd90694cb83b12f8627ef5e09018a60ddc5960bc
Noure <101957571+Nourezzz@users.noreply.github.com>
2023-04-24 01:48:06 +0200

    Update RegistrazioneController.java

--- src/main/java/it/corso/controller/RegistrazioneController.java
+++ src/main/java/it/corso/controller/RegistrazioneController.java
@@ -9,9 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import it.corso.model.Anagrafica;
-import it.corso.model.Profilo;
 import it.corso.service.AnagraficaService;
-import it.corso.service.ProfiloService;
+
 
 @Controller
 @RequestMapping("/registrazione")
@@ -20,28 +19,22 @@ public class RegistrazioneController {
 	@Autowired
 	private AnagraficaService anagraficaService;
 	
-	@Autowired
-	private ProfiloService profiloService;
-	
 	@GetMapping
 	public String getPage(
 			Model model,
 			@RequestParam(name = "id", required = false) Integer id)
 	{
-		Anagrafica anagrafica = id == null ? new Anagrafica() : anagraficaService.getAnagraficaByID(id);
+		Anagrafica anagrafica = id == null ? new Anagrafica() : anagraficaService.getAnagraficaById(id);
 		model.addAttribute("anagrafica", anagrafica);
-		Profilo profilo = id == null ? new Profilo() : profiloService.getProfiloById(id);
-		model.addAttribute("profilo", profilo);
 		return "registrazione";
 	}
 	
 	@PostMapping
-	public String registraAnagrafica(@ModelAttribute("anagrafica") Anagrafica anagrafica,
-			@ModelAttribute("profilo") Profilo profilo)
+	public String registraAnagrafica(@ModelAttribute("anagrafica") Anagrafica anagrafica
+			)
 	{
 		anagraficaService.registraAnagrafica(anagrafica);
-		profiloService.registraProfilo(profilo);
-		return "redirect:/login";
+		return "redirect:/utente_login";
 	}
 	
 }
